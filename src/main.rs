160e91e213ded681917361e49f4a774056204d2e
Nathan Stocks <cleancut@github.com>
2022-10-22 13:51:33 -0600

    add little guy sprite

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::texture::ImageSettings};
 
 fn main() {
     App::new()
+        .insert_resource(ImageSettings::default_nearest()) // prevents blurry sprites
         .insert_resource(WindowDescriptor {
-            title: "Journey to (Somewhere?)".into(),
+            title: "Journey to Find (something?)".into(),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
@@ -11,6 +12,17 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+#[derive(Component)]
+struct Player;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // 2D camera
     commands.spawn_bundle(Camera2dBundle::default());
+
+    // Player
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("little_guy.png"),
+        transform: Transform::from_scale(Vec3::new(4.0, 4.0, 4.0)),
+        ..Default::default()
+    });
 }
