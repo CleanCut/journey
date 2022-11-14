9507e293d9bcb2058998734b9602a2a7903e255e
Nathan Stocks <cleancut@github.com>
2022-11-13 19:32:04 -0700

    upgrade to bevy 0.9

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use bevy::{input::gamepad::GamepadSettings, prelude::*, render::texture::ImageSettings};
+use bevy::{input::gamepad::GamepadSettings, prelude::*};
 use bevy_ecs_tilemap::prelude::*;
 use leafwing_input_manager::prelude::*;
 
 fn main() {
     App::new()
-        .insert_resource(ImageSettings::default_nearest()) // prevents blurry sprites
-        .insert_resource(WindowDescriptor {
-            title: "Journey to Find (something?)".into(),
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                // Set the window title
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Journey to Find (something?)".into(),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                // Prevent blurry sprites
+                .set(ImagePlugin::default_nearest())
+                // Enable asset hot loading
+                .set(AssetPlugin {
+                    watch_for_changes: true,
+                    ..Default::default()
+                }),
+        )
         .add_plugin(InputManagerPlugin::<Action>::default())
         .add_plugin(TilemapPlugin)
         .add_startup_system(setup)
@@ -55,11 +67,11 @@ enum Action {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // 2D camera
-    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default());
 
     // Player
-    commands
-        .spawn_bundle(SpriteBundle {
+    commands.spawn((
+        SpriteBundle {
             texture: asset_server.load("little_guy.png"),
             transform: Transform {
                 translation: Vec3::new(0.0, 0.0, 1.0),
@@ -67,8 +79,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ..Default::default()
             },
             ..Default::default()
-        })
-        .insert_bundle(InputManagerBundle::<Action> {
+        },
+        InputManagerBundle::<Action> {
             action_state: ActionState::default(),
             input_map: InputMap::default()
                 .insert(DualAxis::left_stick(), Action::Move)
@@ -76,8 +88,9 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 .insert(VirtualDPad::arrow_keys(), Action::Move)
                 .insert(VirtualDPad::wasd(), Action::Move)
                 .build(),
-        })
-        .insert(Player);
+        },
+        Player,
+    ));
 
     // Animated planet
     spawn_planet(&mut commands, &asset_server);
@@ -86,18 +99,17 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn spawn_planet(commands: &mut Commands, asset_server: &Res<AssetServer>) {
     let texture_handle: Handle<Image> = asset_server.load("Planet.png");
 
-    let tilemap_entity = commands.spawn().id();
+    let tilemap_entity = commands.spawn_empty().id();
 
     let size = TilemapSize { x: 1, y: 1 };
     let grid_size = TilemapGridSize { x: 16.0, y: 14.0 };
     let mut tile_storage = TileStorage::empty(size);
 
     let tile_entity = commands
-        .spawn()
-        .insert_bundle(TileBundle {
+        .spawn(TileBundle {
             position: TilePos::new(0, 0),
             tilemap_id: TilemapId(tilemap_entity),
-            texture: TileTexture(0),
+            texture_index: TileTextureIndex(0),
             ..Default::default()
         })
         .id();
@@ -110,24 +122,20 @@ fn spawn_planet(commands: &mut Commands, asset_server: &Res<AssetServer>) {
         speed: 0.075,
     });
 
-    commands
-        .entity(tilemap_entity)
-        .insert_bundle(TilemapBundle {
-            grid_size,
-            map_type: TilemapType::Square {
-                diagonal_neighbors: false,
-            },
-            size,
-            storage: tile_storage,
-            texture: TilemapTexture::Single(texture_handle),
-            tile_size: TilemapTileSize { x: 112.0, y: 112.0 },
-            transform: Transform {
-                translation: Vec3::new(-325.0, 75.0, 0.0),
-                scale: Vec3::splat(4.0),
-                ..Default::default()
-            },
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type: TilemapType::Square,
+        size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size: TilemapTileSize { x: 112.0, y: 112.0 },
+        transform: Transform {
+            translation: Vec3::new(-325.0, 75.0, 0.0),
+            scale: Vec3::splat(4.0),
             ..Default::default()
-        });
+        },
+        ..Default::default()
+    });
 }
 
 const MOVE_SPEED: f32 = 200.0;
@@ -147,6 +155,10 @@ fn movement(
 
 fn configure_gamepads(mut settings: ResMut<GamepadSettings>) {
     // add a larger default dead-zone to all axes (ignore small inputs, round to zero)
-    settings.default_axis_settings.negative_low = -0.125;
-    settings.default_axis_settings.positive_low = 0.125;
+    settings
+        .default_axis_settings
+        .set_deadzone_lowerbound(-0.125);
+    settings
+        .default_axis_settings
+        .set_deadzone_upperbound(0.125);
 }
